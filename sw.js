b62a86b8f4adc03c9dd5c55c0578051332bb4d09
zezo20234 <zezo_z4ever@outlook.com>
2026-05-29 20:24:54 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,10 @@
-.
+const CACHE_NAME = 'realm-cache-v1';
+const ASSETS = ['./', './index.html'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+});
